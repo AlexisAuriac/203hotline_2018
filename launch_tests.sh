e91ee4657a2b33f4cb0029f7bf5d27dc34a64ea8
alexis <alexis.auriac@epitech.eu>
2019-03-18 01:24:26 +0100

    Reorganized test files

---
+++ launch_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# CONSTANTS
+readonly TEST_DIR='./tests'
+
+readonly TESTS=(
+    $TEST_DIR/tests_params.rb
+    $TEST_DIR/tests_utilities.rb
+)
+
+readonly INTERPRET='ruby'
+
+
+# EXECUTE TESTS
+for i in ${TESTS[@]}; do
+    $INTERPRET $i
+    echo
+done
---
+++ tests/tests_utilities.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require_relative "../Utilities.rb"
+
+EXIT_FAILURE = 84
+EXIT_SUCCESS = 0
+
+class TestUtilities < Minitest::Test
+    def test_factorial
+        assert_equal(1, Utilities.factorial(0))
+        assert_equal(1, Utilities.factorial(1))
+        assert_equal(120, Utilities.factorial(5))
+        assert_equal(12146304367025329675766243241881295855454217088483382315328918161829235892362167668831156960612640202170735835221294047782591091570411651472186029519906261646730733907419814952960000000000000000000000000000, Utilities.factorial(123))
+    end
+
+    def test_binCoef
+        assert_equal(3, Utilities.binCoef(3, 2))
+        assert_equal(30664510802988208300, Utilities.binCoef(100, 18))
+        assert_equal(1, Utilities.binCoef(50, 50))
+    end
+end
