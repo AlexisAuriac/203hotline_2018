44cfcdd2518025a99fbdb382ec0c44daee538bf1
alexis <alexis.auriac@epitech.eu>
2019-03-21 20:30:59 +0100

    Added new tests

--- tests/tests_examples.rb
+++ tests/tests_examples.rb
@@ -6,6 +6,7 @@ EXIT_SUCCESS = 0
 
 RES1 = "./tests/res_ex1.txt"
 RES2 = "./tests/res_ex2.txt"
+RES3 = "./tests/res_ex3.txt"
 
 R_COMPUTATION_TIME = /^computation time:  \d+\.\d{2} ms$/
 
@@ -35,6 +36,31 @@ class TestSubjectExample < Minitest::Test
         end
         $stdout = STDOUT
         expected = File.read(RES2)
+
+        output = output.string.split("\n")
+        expected = expected.split("\n")
+
+        assert_equal(expected.length, output.length, "different lengths")
+        for i in 0..expected.length
+            if /^computation/ =~ expected[i]
+                assert_match(R_COMPUTATION_TIME, output[i], "diff at line #{i}")
+            else
+                assert_equal(expected[i], output[i], "diff at line #{i}")
+            end
+        end
+    end
+
+    def test_distrib
+        output = StringIO.new()
+        $stdout = output
+        begin
+            main(["380"])
+            raise
+        rescue SystemExit => e
+            assert_equal(EXIT_SUCCESS, e.status)
+        end
+        $stdout = STDOUT
+        expected = File.read(RES3)
 
         output = output.string.split("\n")
         expected = expected.split("\n")
